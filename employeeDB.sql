33527eb5e3a0844009339ffb9718574c0ff833f8
Ctucker9233 <court.m.tucker@gmail.com>
2019-12-13 19:49:05 -0800

    Added database schema and table data

---
+++ employeeDB.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(30) not null,
+  PRIMARY KEY (id)
+);
+
+create table employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(30),
+    salary DECIMAL,
+    department_id int,
+    index department_id (department_id),
+    FOREIGN KEY (department_id) REFERENCES department(id) on DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id int,
+    manager_id int,
+    index role_id (role_id),
+    index manager_id (manager_id),
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) on DELETE CASCADE,
+    FOREIGN KEY manager_id REFERENCES employee(id) on DELETE CASCADE,
+    PRIMARY KEY (id)
+)
